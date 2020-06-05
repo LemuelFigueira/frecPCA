11fee718fbbfa05af524f05daea2c58fe14083c8
wallacez1 <wallace_silva@unigranrio.br>
2020-06-05 19:45:40 -0300

    endereço e horario

--- front_pca/src/components/CreateRoom.vue
+++ front_pca/src/components/CreateRoom.vue
@@ -1,6 +1,12 @@
 <template>
   <v-row justify="center">
-    <v-dialog v-model="show" fullscreen hide-overlay transition="dialog-bottom-transition">
+    <v-dialog
+      v-model="show"
+      fullscreen
+      width="unset"
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
       <template v-slot:activator="{ on }">
         <v-btn color="deep-purple" dark v-on="on">Open Dialog</v-btn>
       </template>
@@ -15,14 +21,48 @@
             <v-btn dark text @click="submit">Criar!</v-btn>
           </v-toolbar-items>
         </v-toolbar>
-        <v-container class="grey lighten-5 pt-12">
+        <v-container>
           <v-col cols="12" md="10" xs="10" lg="10">
-            <v-file-input  label="Imagem do evento"  accept="image/png, image/jpeg, image/bmp, image/jpeg" :rules="rules" v-model="roomPic" filled prepend-icon="mdi-camera"></v-file-input>
+            <v-file-input
+              label="Imagem do evento"
+              accept="image/png, image/jpeg, image/bmp, image/jpeg"
+              :rules="rules"
+              v-model="roomPic"
+              filled
+              prepend-icon="mdi-camera"
+            ></v-file-input>
             <form>
+              <v-menu
+                ref="menu"
+                v-model="menu2"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                :return-value.sync="time"
+                transition="scale-transition"
+                offset-y
+                max-width="290px"
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="time"
+                    label="Picker in menu"
+                    prepend-icon="access_time"
+                    readonly
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-time-picker
+                  v-if="menu2"
+                  v-model="time"
+                  full-width
+                  @click:minute="$refs.menu.save(time)"
+                ></v-time-picker>
+              </v-menu>
               <v-text-field
                 v-model="name"
                 :error-messages="nameErrors"
-                :counter="20"
+                :counter="50"
                 label="Nome"
                 required
               ></v-text-field>
@@ -30,8 +70,12 @@
                 label="Descrição"
                 v-model="descripton"
                 :error-messages="descriptionErrors"
-                :counter="50"
+                :counter="100"
               ></v-text-field>
+              <v-text-field label="Endereço" v-model="adress" :error-messages="adressErrors"></v-text-field>
+              <v-text-field label="Cidade" v-model="city" :error-messages="cityErrors"></v-text-field>
+              <v-text-field label="Bairro" v-model="district" :error-messages="districtErrors"></v-text-field>
+
               <v-select
                 v-model="quantidadeParticipantes"
                 :error-messages="participantsErrors"
@@ -52,17 +96,30 @@
 
 <script>
 import { required, maxLength } from "vuelidate/lib/validators";
+// import VueTimepicker from "vue2-timepicker";
+// import "vue2-timepicker/dist/VueTimepicker.css";
 export default {
   validations: {
-    name: { required, maxLength: maxLength(20) },
-    descripton: { required, maxLength: maxLength(50) },
+    name: { required, maxLength: maxLength(50) },
+    descripton: { required, maxLength: maxLength(100) },
+    adress: { required },
+    district: { required },
+    city: { required },
     quantidadeParticipantes: { required }
   },
+
   data() {
     return {
+      time: null,
+      menu2: false,
+      modal2: false,
+      errorMessage: "",
       quantidade: [10, 50, 100, 300, 500, 1000],
       name: "",
-      roomPic:null,
+      adress: "",
+      city: "",
+      district: "",
+      roomPic: null,
       descripton: "",
       quantidadeParticipantes: "",
       dialog: false,
@@ -86,19 +143,16 @@ export default {
       this.$v.$touch();
       const result = await this.$v.$anyError;
 
-      let fileType = this.roomPic.type.split("/")
-      if(!result && fileType[0] === 'image'){
-       
-        console.log(fileType[0])
-        console.log(this.roomPic)
-        console.log(this.quantidadeParticipantes)
-        console.log(this.name)
-        console.log(this.descripton)
-       
-      }else if(fileType[0] !== 'image'){
-        
+      let fileType = this.roomPic.type.split("/");
+      if (!result && fileType[0] === "image") {
+        console.log(fileType[0]);
+        console.log(this.roomPic);
+        console.log(this.quantidadeParticipantes);
+        console.log(this.name);
+        console.log(this.descripton);
+      } else if (fileType[0] !== "image") {
+        console.log("erro");
       }
-    
     }
   },
   computed: {
@@ -116,15 +170,36 @@ export default {
       const errors = [];
       if (!this.$v.name.$dirty) return errors;
       !this.$v.name.maxLength &&
-        errors.push("Nome do evento deve ter no máximo 20 caracteres ");
+        errors.push("Nome do evento deve ter no máximo 50 caracteres ");
       !this.$v.name.required && errors.push("Defina o nome do evento");
+      return errors;
+    },
+    cityErrors() {
+      const errors = [];
+      if (!this.$v.city.$dirty) return errors;
+
+      !this.$v.city.required && errors.push("Defina a cidade do evento");
+      return errors;
+    },
+    adressErrors() {
+      const errors = [];
+      if (!this.$v.name.$dirty) return errors;
+
+      !this.$v.name.required && errors.push("Defina o endereço do evento");
+      return errors;
+    },
+    districtErrors() {
+      const errors = [];
+      if (!this.$v.name.$dirty) return errors;
+
+      !this.$v.name.required && errors.push("Defina o bairro do evento");
       return errors;
     },
     descriptionErrors() {
       const errors = [];
       if (!this.$v.descripton.$dirty) return errors;
       !this.$v.descripton.maxLength &&
-        errors.push("Descrição do evento deve ter no máximo 50 caracteres ");
+        errors.push("Descrição do evento deve ter no máximo 100 caracteres ");
       !this.$v.descripton.required &&
         errors.push("Crie uma descrição do evento");
       return errors;
@@ -136,7 +211,6 @@ export default {
         errors.push("Selecione a quantidade de participantes");
       return errors;
     }
-    
   }
 };
 </script>
